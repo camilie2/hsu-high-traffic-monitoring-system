a9dc40559dacd180004e79fb863dff60ebf7db5c
fuzzyter <165622596+fuzzyter@users.noreply.github.com>
2024-11-22 12:02:49 +0900

    setAlert

---
+++ src/pages/Start.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Start() {
+    return(
+      <BG>
+        <TopBox>
+          <UserView></UserView>
+        </TopBox>
+        
+        <View1></View1>
+      </BG>
+      
+  
+    );
+  }
+  
+  export default Start;
